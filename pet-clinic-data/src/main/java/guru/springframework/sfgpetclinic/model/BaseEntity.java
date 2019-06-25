64424999a0c85257e022c39980d9d261d1060daf
doa-b <doa.bonifacio@gmail.com>
2019-06-25 13:45:25 +0200

    Implement Base Entity. Closes #34

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,12 +1,21 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * Created by Doa on 21-5-2019.
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+    //typically used to manage ID and create/update timestamp fields
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Underlying SQL and H2 database will use auto increment field
+                                                        // this setting retrieves that ID for us
     private Long id;
 
     public Long getId() {
