8fc645e7ef6777f6e43c44f24afa6c2035f38546
doa-b <doa.bonifacio@gmail.com>
2019-06-25 14:39:12 +0200

    Convert Owners to JPA Entities. Closes #35

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,13 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * Created by Doa on 21-5-2019.
  */
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER) // default for many to many is lazy
+    @JoinTable(name = "vet_specialies", joinColumns = @JoinColumn(name = "vet_id"), // unidirectional, so no need to specify in Speciality class
+    inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialties = new HashSet<>(); // good to initialize so we can add entities to it, without creating it first
 
     public Set<Speciality> getSpecialities() {
