dfaadc299eb862c38286b4da5245bd56917a47d5
doa-b <doa.bonifacio@gmail.com>
2019-06-25 14:01:12 +0200

    Convert Owners to JPA Entities. Closes #35

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -1,11 +1,18 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
 /**
  * Created by Doa on 21-5-2019.
  */
+@MappedSuperclass
 public class Person extends BaseEntity{
 
+    @Column(name = "first_name") // hybernate does this automatically: convert camelcase member to snake case table name
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
     public String getFirstName() {
